6e0dc6daae7f4c3a985cd779b9c9200bb890c555
HITESH S P <hiteshsp2004@gmail.com>
2025-08-26 13:46:52 +0530

    Secure-frontend

---
+++ src/pages/student/StudentSideBar.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import {
+  Divider,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+
+import HomeIcon from "@mui/icons-material/Home";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AnnouncementOutlinedIcon from "@mui/icons-material/AnnouncementOutlined";
+import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+const StudentSideBar = () => {
+  const location = useLocation();
+  return (
+    <>
+      <React.Fragment>
+        <ListItemButton component={Link} to="/">
+          <ListItemIcon>
+            <HomeIcon
+              color={
+                location.pathname === ("/" || "/Student/dashboard")
+                  ? "primary"
+                  : "inherit"
+              }
+            />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItemButton>
+
+        <ListItemButton component={Link} to="/logout">
+          <ListItemIcon>
+            <ExitToAppIcon
+              color={
+                location.pathname.startsWith("/logout") ? "primary" : "inherit"
+              }
+            />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItemButton>
+      </React.Fragment>
+    </>
+  );
+};
+
+export default StudentSideBar;
